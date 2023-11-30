c1425bd0597efcef6ddf8960316141a60e8f60d7
Иларион Ерченко <turolretar@gmail.com>
2023-11-30 13:07:59 +0300

    add vite and typescript configuration

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vuetify from 'vite-plugin-vuetify';
+
+export default defineConfig({
+    plugins: [vue(), vuetify()],
+    resolve: {
+        alias: {
+            '@': resolve('./src'),
+        },
+    },
+});
