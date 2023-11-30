a78125edeb6cf37e791f2dc2427b5a313c2b743f
Иларион Ерченко <turolretar@gmail.com>
2023-11-30 13:08:52 +0300

    add component structure

---
+++ src/components/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+
+<template>
+    <v-app>
+        <v-app-bar :elevation="2">App</v-app-bar>
+        <v-container> </v-container>
+    </v-app>
+</template>
+
+<style scoped lang="scss">
+.container {
+    max-width: 1280px;
+    margin: 0 auto;
+    padding: 2rem;
+    text-align: center;
+}
+</style>
---
+++ src/components/ChatWindow.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div class="container">Hello world!</div>
+</template>
+
+<style scoped lang="scss">
+.container {
+    max-width: 1280px;
+    margin: 0 auto;
+    padding: 2rem;
+    text-align: center;
+}
+</style>
