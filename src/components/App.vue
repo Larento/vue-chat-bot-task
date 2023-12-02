a3dba1009a2dc6964b5d27051942603102c00849
Иларион Ерченко <turolretar@gmail.com>
2023-12-03 00:04:17 +0300

    add App component

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,17 +1,13 @@
-<script setup lang="ts"></script>
-
 <template>
-    <v-app>
-        <v-app-bar :elevation="2">App</v-app-bar>
-        <v-container> </v-container>
+    <v-app class="app">
+        <toolbar></toolbar>
+        <v-container>
+            <chat-window></chat-window>
+        </v-container>
     </v-app>
 </template>
 
-<style scoped lang="scss">
-.container {
-    max-width: 1280px;
-    margin: 0 auto;
-    padding: 2rem;
-    text-align: center;
-}
-</style>
+<script setup lang="ts">
+import Toolbar from '@/components/Toolbar.vue';
+import ChatWindow from '@/components/ChatWindow.vue';
+</script>
