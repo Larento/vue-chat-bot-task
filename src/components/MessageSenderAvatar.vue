2d887eb92674b55e2d352bb0845112b3d5d35688
Иларион Ерченко <turolretar@gmail.com>
2023-12-03 00:05:38 +0300

    add MessageSenderAvatar component for displaying an appropriate icon for sender

---
+++ src/components/MessageSenderAvatar.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-avatar
+        :icon="isBotSender ? 'mdi-robot-happy' : 'mdi-account'"
+        :color="isBotSender ? 'indigo-darken-1' : 'indigo-lighten-4'"
+        variant="tonal"
+        class="pa-2 flex-shrink-0 align-self-end w-100 h-100"
+    >
+    </v-avatar>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+    /** Indicates, whether the sender is a bot or not. */
+    isBotSender: boolean;
+}>();
+</script>
