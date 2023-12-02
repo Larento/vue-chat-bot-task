5b88ad849ffc4c9a05300e18755a2359327452ec
Иларион Ерченко <turolretar@gmail.com>
2023-12-03 00:04:43 +0300

    add Toolbar component

---
+++ src/components/Toolbar.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-toolbar :elevation="0" flat color="indigo-lighten-4" class="toolbar">
+        <v-container fluid class="d-flex ga-2 flex-row align-center toolbar-container">
+            <v-toolbar-title class="text-h6 font-weight-black"> Чат-бот </v-toolbar-title>
+        </v-container>
+    </v-toolbar>
+</template>
+
+<style scoped lang="scss">
+.toolbar {
+    background-image: linear-gradient(-10deg, #c5cae9, #e8eaf6);
+}
+
+.toolbar-container {
+    max-width: 80ch;
+}
+</style>
