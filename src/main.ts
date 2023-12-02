dc3c135d014e4be1f079ea2482b37a88b2ec7efd
Иларион Ерченко <turolretar@gmail.com>
2023-12-02 23:58:20 +0300

    add material design icons

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import { createVuetify } from 'vuetify';
 import 'vuetify/styles';
+import '@mdi/font/css/materialdesignicons.css';
 
 import App from '@/components/App.vue';
 
