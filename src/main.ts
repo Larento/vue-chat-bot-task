559068fe18490a52dc00cf4b57c638a6d50c5f1c
Иларион Ерченко <turolretar@gmail.com>
2023-12-02 23:57:41 +0300

    remove global stylesheet

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { createVuetify } from 'vuetify';
 import 'vuetify/styles';
 
 import App from '@/components/App.vue';
-import '@/styles/global.scss';
 
 const vuetify = createVuetify();
 createApp(App).use(vuetify).mount('#app');
