a8abd054ead0dd057df3b2434f373687762f629d
Иларион Ерченко <turolretar@gmail.com>
2023-12-04 13:46:08 +0300

    add scroll to bottom of chat window, when chat height exceeds viewport height

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -1,30 +1,45 @@
 <template>
-    <v-layout class="d-block mx-auto chat-window-container">
-        <transition-group name="fade">
-            <v-layout
-                v-for="(message, index) in store.messages"
-                :key="index"
-                :class="isBotSender(message) ? 'flex-row' : 'flex-row-reverse'"
-                class="d-flex flex-nowrap ga-2 py-1 px-0 ma-0"
-            >
-                <div class="avatar-container">
-                    <message-sender-avatar
-                        v-if="store.isPreviousMessageFromDifferentSender(index)"
-                        :is-bot-sender="isBotSender(message)"
-                    ></message-sender-avatar>
-                </div>
-                <message-bubble
-                    :message="message"
-                    :type-speed="isBotSender(message) ? 30 : 20"
-                ></message-bubble>
-            </v-layout>
-        </transition-group>
+    <div class="w-100 overflow-auto scrollable-container">
+        <div ref="containerRef" class="mx-auto chat-window-container">
+            <v-container>
+                <transition-group name="fade">
+                    <v-layout
+                        v-for="(message, index) in store.messages"
+                        :key="index"
+                        :class="isBotSender(message) ? 'flex-row' : 'flex-row-reverse'"
+                        class="d-flex flex-nowrap ga-2 py-2"
+                    >
+                        <div class="align-self-start avatar-container">
+                            <message-sender-avatar
+                                v-show="store.isPreviousMessageFromDifferentSender(index)"
+                                :is-bot-sender="isBotSender(message)"
+                            ></message-sender-avatar>
+                        </div>
+                        <div class="align-self-start message-bubble-container">
+                            <message-bubble
+                                :message="message"
+                                :type-speed="isBotSender(message) ? 30 : 20"
+                            ></message-bubble>
+                        </div>
+                    </v-layout>
+                </transition-group>
+                <div
+                    class="d-flex flex-wrap justify-center align-start pt-4 ga-4 position-relative"
+                >
                     <transition-group @after-leave="handleCommandButtonsAfterLeave" name="fade">
-                    v-for="(command, index) in store.commandList"
+                        <command-button
+                            :key="command"
+                            v-for="(command, index) in store.commandList"
                             v-visible="!store.currentCommand"
+                            :style="{ '--index': index }"
+                            :command="command"
+                        >
+                        </command-button>
                     </transition-group>
+                </div>
+            </v-container>
         </div>
-    </v-layout>
+    </div>
 </template>
 
 <script setup lang="ts">
@@ -51,12 +66,30 @@ const handleCommandButtonsAfterLeave = computed(() => {
     };
 });
 
+const containerRef = ref<VNodeRef | null>(null);
+const resizeObserver = ref(
+    new ResizeObserver((entries) => {
+        for (const entry of entries) {
+            entry.target?.scrollIntoView({ block: 'end', behavior: 'auto' });
+        }
+    }),
+);
+
+onMounted(() => {
+    if (containerRef.value) resizeObserver.value.observe(containerRef.value);
+});
+
 setTimeout(() => store.executeCommand(), 300);
 </script>
 
 <style scoped lang="scss">
+.scrollable-container {
+    scroll-behavior: smooth;
+}
+
 .chat-window-container {
     max-width: 80ch;
+    min-height: 100%;
 }
 
 .avatar-container {
