20a34f7c129e897ae3116d5823e02f9c11c7e1cf
Иларион Ерченко <turolretar@gmail.com>
2023-12-04 13:44:12 +0300

    rework command buttons transition leave handler

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -19,20 +19,17 @@
                 ></message-bubble>
             </v-layout>
         </transition-group>
-        <div class="d-flex flex-wrap justify-center pt-4 ga-4 position-relative">
-            <transition-group name="fade" @after-leave="handleCommandButtonAfterLeave">
-                <command-button
+                    <transition-group @after-leave="handleCommandButtonsAfterLeave" name="fade">
                     v-for="(command, index) in store.commandList"
                             v-visible="!store.currentCommand"
-                    @before-set-command="isCommandAlreadyPosted = false"
-                >
-                </command-button>
-            </transition-group>
+                    </transition-group>
         </div>
     </v-layout>
 </template>
 
 <script setup lang="ts">
+import { computed, ref, onMounted, type VNodeRef } from 'vue';
+
 import { type Message } from '@/modules/chat';
 import { store } from '@/modules/store';
 
@@ -40,12 +37,19 @@ import MessageSenderAvatar from '@/components/MessageSenderAvatar.vue';
 import MessageBubble from '@/components/MessageBubble.vue';
 import CommandButton from '@/components/CommandButton.vue';
 
-const isCommandAlreadyPosted = ref(false);
 const isBotSender = (message: Message) => message.sender === 'bot';
-const handleCommandButtonAfterLeave = () => {
-    if (!isCommandAlreadyPosted.value) store.postCommand();
-    isCommandAlreadyPosted.value = true;
-};
+
+const handleCommandButtonsAfterLeave = computed(() => {
+    let numberOfCalls = 0;
+    const numberOfButtons = store.commandList.length;
+    return () => {
+        numberOfCalls += 1;
+        if (numberOfCalls === numberOfButtons) {
+            store.postCommand();
+            numberOfCalls = 0;
+        }
+    };
+});
 
 setTimeout(() => store.executeCommand(), 300);
 </script>
